e7cf21fc1dc31cfeea1bdcf01ff25a40981102e2
Nirmal Fernando <fernandonirmal345@gmail.com>
2026-01-30 22:39:10 +0530

    feat: add batch stock model, migration, and API endpoints.

---
+++ database/migrations/2026_01_30_165732_create_batch__stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batch__stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('supplier_invoice_id')->constrained('suppliers');
+            $table->integer('no_cases');
+            $table->integer('pack_size');
+            $table->integer('qty');
+            $table->decimal('retail_price', 10, 2);
+            $table->decimal('netprice', 10, 2);
+            $table->date('expiry_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batch__stocks');
+    }
+};
